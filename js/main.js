3a72fc6418db43777b15b07e17bdf2b42768ab11
David Mensah <david.mensah@hubtel.com>
2024-05-30 10:10:53 +0000

    Built the navigation bar and its interactions

--- js/main.js
+++ js/main.js
@@ -10,6 +10,47 @@ $(window).on("load", function () {
 });
 
 $(document).ready(function () {
+    const menu = $(".davnavbarmenu");
+    const closeBtn = $(".davnavbarclose");
+    const hamburgerBtn = $(".davnavbarhamburger");
+    const davnavbaritem = $(".davnavbaritem");
+    const davnavbarlinks = $(".davnavbarlink");
+    const davnavbarsocialicon = $(".davnavbarsocialicon");
 
-    // Code to be executed when the document is ready
+    if (hamburgerBtn.length) {
+        hamburgerBtn.on("click", function () {
+            menu.css("left", "0px");
+            davnavbaritem.css({
+                "transform": "translateX(0px)",
+                "visibility": "visible"
+            });
+            davnavbarsocialicon.css({
+                "transform": "translateX(0px)",
+                "visibility": "visible"
+            });
+        });
+    }
+
+    if (closeBtn.length) {
+        closeBtn.on("click", function () {
+            menu.css("left", "-100%");
+            davnavbaritem.css({
+                "transform": "translateX(-150px)",
+                "visibility": "hidden"
+            });
+            davnavbarsocialicon.css({
+                "transform": "translateX(-150px)",
+                "visibility": "hidden"
+            });
+        });
+    }
+
+    // for each navbarlink clicked
+    davnavbarlinks.each(function () {
+        $(this).on("click", function () {
+            $(this).addClass("davnavbaractivelink");
+            davnavbarlinks.not(this).removeClass("davnavbaractivelink");
+        });
+    });
+
 });
